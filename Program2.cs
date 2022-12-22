7ee789e93dc5c801868daba6d3d3ae618c5553ef
Настя Соловьева <121259111+asoloveva123@users.noreply.github.com>
2022-12-22 23:45:47 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static string Perevorot(string str)
+    {
+        string temp = "";
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            temp += str[i];
+        }
+        return temp;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите текст: ");
+        string str = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Текст наоборот: {0}", Perevorot(str));
+        Console.ReadKey();
+    }
+}
